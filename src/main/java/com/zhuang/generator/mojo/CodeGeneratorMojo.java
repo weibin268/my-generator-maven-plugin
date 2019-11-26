ba95eeb85cc2f93f11eb0c5b93bd0c4a35d95f77
zwb <zhuangweibin@stbl.cc>
2019-11-26 17:43:33 +0800

    aa

--- src/main/java/com/zhuang/generator/mojo/CodeGeneratorMojo.java
+++ src/main/java/com/zhuang/generator/mojo/CodeGeneratorMojo.java
@@ -6,24 +6,31 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.util.Map;
 
 @Mojo(name = "generate")
 public class CodeGeneratorMojo extends AbstractMojo {
 
-    private MyGeneratorProperties myGeneratorProperties;
 
-    public CodeGeneratorMojo() {
-        myGeneratorProperties = new MyGeneratorProperties();
-    }
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    private File outputDirectory;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        try {
-            Class<?> codeGeneratorClass = Class.forName(myGeneratorProperties.getImplementClass());
-            CodeGenerator codeGenerator = (CodeGenerator) codeGeneratorClass.getConstructor().newInstance();
-            codeGenerator.generate();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+
+        System.out.println(outputDirectory.getAbsoluteFile());
+
+
+//        MyGeneratorProperties myGeneratorProperties = new MyGeneratorProperties();
+//        try {
+//            Class<?> codeGeneratorClass = Class.forName(myGeneratorProperties.getImplementClass());
+//            CodeGenerator codeGenerator = (CodeGenerator) codeGeneratorClass.getConstructor().newInstance();
+//            codeGenerator.generate();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
