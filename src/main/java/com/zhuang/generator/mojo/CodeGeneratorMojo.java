edf70aa7eef0988d156bede9552dead0d4be6c46
zwb <zhuangweibin@stbl.cc>
2019-11-26 18:01:31 +0800

    aa

--- src/main/java/com/zhuang/generator/mojo/CodeGeneratorMojo.java
+++ src/main/java/com/zhuang/generator/mojo/CodeGeneratorMojo.java
@@ -5,24 +5,28 @@ import com.zhuang.generator.config.MyGeneratorProperties;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.util.Map;
 
-@Mojo(name = "generate")
+
+@Mojo(name = "generate",defaultPhase = LifecyclePhase.COMPILE,requiresProject = true)
 public class CodeGeneratorMojo extends AbstractMojo {
 
 
     @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
     private File outputDirectory;
 
+    @Parameter( defaultValue = "${project.build.resources}")
+    private File resources;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
         System.out.println(outputDirectory.getAbsoluteFile());
-
 
 //        MyGeneratorProperties myGeneratorProperties = new MyGeneratorProperties();
 //        try {
